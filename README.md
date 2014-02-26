a7510505c90c30ff0602b87a600d9e6f7f2d693a
Nicholas Johnson <nicholas@forwardadvance.com>
2014-02-26 17:45:31 +0000

    New readme and code

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-truncate
+Truncate
 ========
 
-Easily truncate long blocks of text.
+Easily and unobtrusively truncate long blocks of text, respecting html.
+
+## Usage
+
+Import jquery and truncate, then simply add a data-truncate attribute to your text, like so:
+
+    <div data-truncate>
+      One long and complex piece of rambling text
+    </div>
+
+Your text will be truncated at 50 words and a more link will be cleanly appended to the last element.
+
+## Options
+
+You can also specify the number of words to truncate at:
+
+    <div data-truncate="12">
+      One long and complex piece of rambling text...
+    </div>
+
+## Demo
+
+See it in action here: <http://www.streetbank.com/blog_posts/55#comments>
---
+++ truncate.js
@@ -0,0 +1,43 @@
+/**
+Easily and unobtrusively truncate long blocks of text with a more link.
+Simply add a data-truncate attribute to any element you would like to truncate.
+Nicholas Johnson - www.nicholasjohnson.com
+for
+Streetbank - www.streetbank.com
+*/
+(function () {
+  $.fn.truncate = function () {
+    this.each(function (length) {
+
+      length = length || 50;
+
+      var el = $(this),
+        content = el.html(),
+        split = _.filter(content.split(" "), function (i) {
+          return i;
+        }),
+        truncated,
+        moreLink;
+
+      if (split.length > length) {
+        truncated = split.slice(0, length).join(" ");
+        moreLink = $('<a href="#"> more...</a>');
+        el.html(truncated);
+        el.find('> :last-child').append(moreLink);
+        moreLink.click(function (e) {
+          el.html(content);
+          e.preventDefault();
+        });
+      }
+    });
+  };
+}());
+
+$(function () {
+  $('[data-truncate]').each(function () {
+    var el = $(this),
+      length = parseInt(el.attr('[data-truncate]'), 10);
+    el.truncate(length);
+  });
+});
+
